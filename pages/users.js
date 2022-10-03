53413ba4742e682e2177524d29ba0f2d814d2d7d
nhquyen2001 <nguyenhuuquyen2001@gmail.com>
2022-10-03 16:29:03 +0700

    pages vs components

--- pages/users.js
+++ pages/users.js
@@ -1,11 +1,12 @@
+import User from "../components/user";
+
 const UserList = ({ users }) => {
   return (
     <>
       <h1>List of users</h1>
       {users.map((user) => (
         <div key={user.id}>
-          <p>{user.name}</p>
-          <p>{user.email}</p>
+          <User user={user}/>
         </div>
       ))}
     </>
