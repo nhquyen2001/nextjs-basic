98e0788cc4694be057ebe0bc41cae68dd23b976c
nhquyen2001 <nguyenhuuquyen2001@gmail.com>
2022-10-03 17:30:27 +0700

    SSG with getStaticPaths

---
+++ pages/posts/[postId].js
@@ -0,0 +1,43 @@
+const Post = ({ post }) => {
+  return (
+    <>
+      <h2>
+        {post.id} {post.title}
+      </h2>
+      <p>{post.body}</p>
+    </>
+  );
+};
+
+export default Post;
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [
+      {
+        params: { postId: "1" },
+      },
+      {
+        params: { postId: "2" },
+      },
+      {
+        params: { postId: "3" },
+      },
+    ],
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const { params } = context;
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.postId}`
+  );
+  const data = await response.json();
+
+  return {
+    props: {
+      post: data,
+    },
+  };
+};
