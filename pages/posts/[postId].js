1427cdb03bef6db52c169c77f1b630377f0b2c5d
nhquyen2001 <nguyenhuuquyen2001@gmail.com>
2022-10-04 10:16:43 +0700

    fetching paths for getStaticPaths

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -12,18 +12,19 @@ const Post = ({ post }) => {
 export default Post;
 
 export const getStaticPaths = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+
+  const paths = data.map((post) => {
+    return {
+      params: {
+        postId: `${post.id}`,
+      },
+    };
+  });
+
   return {
-    paths: [
-      {
-        params: { postId: "1" },
-      },
-      {
-        params: { postId: "2" },
-      },
-      {
-        params: { postId: "3" },
-      },
-    ],
+    paths,
     fallback: false,
   };
 };
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -26,7 +26,7 @@ export const getStaticProps = async () => {
 
   return {
     props: {
-      posts: data.slice(0, 3),
+      posts: data,
     },
   };
 };
